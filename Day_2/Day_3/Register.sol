638f76882a8f035cdc5ab9e06fc746a03ca4498d
Gogo-Eng <progressgogochinda@gmail.com>
2026-02-21 19:43:57 +0100

    faet: implement expel student and sack staff from my management system

--- Day_2/Day_3/Register.sol
+++ Day_2/Day_3/Register.sol
@@ -1,123 +1,131 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
-
 interface IERC20 {
-    function transfer(address to, uint256 amount) external returns (bool);
-    function transferFrom(address from, address to, uint256 amount) external returns (bool);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
+    function transferFrom(address _owner, address _to, uint _value) external returns(bool success);
+    function transfer(address _to, uint256 _value) external returns (bool success);
+    function balanceOf(address _address) external view returns (uint256);               
 }
-
 contract Register {
+    enum PaymentStatus {
+        paid,
+        pending
+    }    
+    enum Status {
+        active,
+        inactive
+    }
+    
     struct Student {
+        string name;
         uint256 id;
-        string  name;
-        address walletAddress;
-        uint256 level;          // 100, 200, 300, 400
-        uint256 feesRequired;   // based on level
-        uint256 feesPaid;
-        uint256 paidAt;         // timestamp
-        bool    status;
+        address studentAddress;
+        uint8 level;
+        string course;
+        PaymentStatus status;
     }
 
-    struct Staff {
+    struct Teacher {
+        string teacherName;
         uint256 id;
-        string  name;
-        address walletAddress;
+        address teacherAddress;
         uint256 salary;
-        uint256 lastPaidAt;     // timestamp
-        bool    status;
-    }                             // deployer = school admin
-
-    mapping(address => Student) students;          // wallet → Student
-    mapping(address => Staff)   staffs;             // wallet → Staff
-    mapping(uint256 => uint256) levelFees;         
-    
-    Student[] public studentList;                           
-    Staff[] public staffList;
-    address[] public studentAddresses;
-    address[] public staffAddresses;
-
-    uint256 studentCount;
-    uint256 staffCount;
-
-    address public owner;
-    IERC20 schoolToken;
-
-    constructor(address _tokenAddress) {
-        schoolToken = IERC20(_tokenAddress);
-
-        owner = msg.sender;
-        levelFees[100] = 100 * 1e18;    
-        levelFees[200] = 150 * 1e18;    
-        levelFees[300] = 200 * 1e18;    
-        levelFees[400] = 250 * 1e18;    
+        Status status;
     }
 
-    function registerStudent(address _address, string memory _name, uint256 _level) public payable returns(bool _success)  {
-        require(_level == 100 || _level == 200 || _level == 300 || _level == 400, "Invalid level");
-        uint256 fee = levelFees[_level];
-        require(fee > 0, "Fee cannot be zero");
+    //Status public status;
+
+    uint8 immutable level_100;
+    uint8 immutable level_200;
+    uint8 immutable level_300;
+    uint8 immutable level_400;
+
+    uint8 immutable level_1;
+    uint8 immutable level_2;
+    uint8 immutable level_3;
+    uint8 immutable level_4;
+
+    Student[] public students;
+    Teacher[] public teachers;
+
+    address[] public studentsAddresses;
+    address[] public teachersAddresses;
+
+    uint8 teacherId;
+    uint studentId;
+
+    event RegistrationSuccessful (address indexed _student, uint256 _amount);
+
+    mapping(address => uint256) public ERCbalance;
+    mapping(uint8 => uint256) public schoolFee;
+    mapping(uint8 => uint256) public teacherSalary;
+
+    address tokenAddress;
+    IERC20 public immutable token;
+
+    constructor(address _address) {
+        tokenAddress = _address;
+        token = IERC20(tokenAddress);
         
-        bool success = schoolToken.transferFrom(_address, address(this), fee);
-        require(success, "Token transfer failed");
-        students[msg.sender] = Student({
-            id: studentCount,
+        schoolFee[level_100] = 1000;
+        schoolFee[level_200] = 2000;
+        schoolFee[level_300] = 3000;
+        schoolFee[level_400] = 4000;
+
+        teacherSalary[level_1] = 500;
+        teacherSalary[level_2] = 1000;
+        teacherSalary[level_3] = 1500;
+        teacherSalary[level_4] = 2000;
+
+    }
+
+
+    function RegisterStudent(string memory _name, uint8 _level, string memory _course) public returns(bool) {
+        uint256 fee = schoolFee[_level];
+
+        require (fee > 0, "can't pay Zero fee");
+        require(_level == 100 || _level == 200 || _level == 300 || _level == 400, "Invalid Level");
+        require(IERC20(tokenAddress).balanceOf(msg.sender) >= fee, "Not enough funds to register");
+        
+        ERCbalance[msg.sender] = ERCbalance[msg.sender] - fee;
+        require(IERC20(tokenAddress).transferFrom(msg.sender, address(this), fee));
+
+        studentId = studentId + 1;
+        Student memory student = Student({
             name: _name,
-            walletAddress: msg.sender,
-            level: _level,
-            feesRequired: fee,
-            feesPaid: fee,
-            paidAt: block.timestamp,
-            status: true
+            id: studentId,
+            studentAddress: msg.sender, 
+            level:_level, course: _course, 
+            status: PaymentStatus.paid
         });
+        students.push(student);
+        studentsAddresses.push(msg.sender);
 
-        studentCount++;
-        studentAddresses.push(msg.sender); 
-        return success;
+        emit RegistrationSuccessful (msg.sender, fee);
+        return true;
     }
 
-    function registerStaff(address staffAddress, string memory _name, uint _salary) public {
-        staffs[staffAddress] = Staff({
-        id:            staffCount,
-        name:          _name,
-        walletAddress: staffAddress,
-        salary:        _salary,
-        lastPaidAt:    0,        // never been paid yet
-        status:        true
-    });
-        
-        staffCount++;
-        staffAddresses.push(staffAddress); 
+    function RegisterTeacher(string memory _name, uint8 _level, address _address) public returns(bool){
+        teacherId ++;
+        teachers.push();
+        Teacher storage teacher = teachers[teachers.length - 1];
+        teacher.teacherName = _name;
+        teacher.id = teacherId;
+        teacher.teacherAddress = _address;
+        teacher.salary = teacherSalary[_level];
+        teacher.status = Status.active;
+
+        teachersAddresses.push(_address);
+
+        return true;
     }
 
-    function payStaff(address staffAddress) external{
-        Staff storage staff = staffs[staffAddress];
-
-        schoolToken.transfer(staffAddress, staff.salary);
-
-        staff.lastPaidAt = block.timestamp;
-    }
-    
-    function getAllStaffs() external view returns (Staff[] memory) {
-        Staff[] memory result = new Staff[](staffAddresses.length);
-
-        for (uint256 i = 0; i < staffAddresses.length; i++) {
-            result[i] = staffs[staffAddresses[i]];
+    function listStudents() public view returns(string memory){
+        string memory all_students = "";
+        for(uint i = 0; i < students.length; i++) {
+            Student storage student = students[i];
+            all_students = student.name;   
         }
-
-        return result;
-    }
-
-    function getAllStudents() external view returns (Student[] memory) {
-        Student[] memory result = new Student[](studentAddresses.length);
-
-        for (uint256 i = 0; i < studentAddresses.length; i++) {
-            result[i] = students[studentAddresses[i]];
-        }
-
-        return result;
+        return all_students;
     }
 
 }
---
+++ Day_7/Exam.sol
@@ -0,0 +1,174 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+interface IERC20 {
+    function transferFrom(address _owner, address _to, uint _value) external returns(bool success);
+    function transfer(address _to, uint256 _value) external returns (bool success);
+    function balanceOf(address _address) external view returns (uint256);               
+}
+contract Register {
+    enum PaymentStatus {
+        paid,
+        pending
+    }    
+    enum Status {
+        active,
+        inactive
+    }
+    
+    struct Student {
+        string name;
+        uint256 id;
+        address studentAddress;
+        uint16 level;
+        string course;
+        Status status;
+        PaymentStatus paymentStatus;
+    }
+
+    struct Staff {
+        string teacherName;
+        uint256 id;
+        address staffAddress;
+        uint256 salary;
+        uint256 lastPaidAt;
+        Status status;
+    }
+
+    Status public status;
+
+    uint16 level_100 = 100;
+    uint16 level_200 = 200;
+    uint16 level_300 = 300;
+    uint16 level_400 = 400;
+
+    Student[] public students;
+    Staff[] public staffs;
+
+    address[] public studentsAddresses;
+    address[] public staffAddresses;
+
+    uint256 staffId;
+    uint256 public studentId;
+    uint256 public fee;
+
+    event RegistrationSuccessful (address indexed _student, uint256 _amount);
+    event PaymetSuccessful (address indexed _teacher, uint256 _amount);
+
+    mapping(address => uint256) public ERCbalance;
+    mapping(uint16 => uint256) public schoolFee;
+    mapping(uint16 => uint256) public staffSalary;
+
+    address tokenAddress;
+    IERC20 public immutable token;
+
+    constructor(address _address) {
+        tokenAddress = _address;
+        token = IERC20(tokenAddress);
+        
+        schoolFee[level_100] = 1000;
+        schoolFee[level_200] = 2000;
+        schoolFee[level_300] = 3000;
+        schoolFee[level_400] = 4000;
+
+        staffSalary[level_100] = 500;
+        staffSalary[level_200] = 1000;
+        staffSalary[level_300] = 1500;
+        staffSalary[level_400] = 2000;
+
+    }
+
+
+    function RegisterStudent(string memory _name, uint16 _level, string memory _course) public returns(bool) {
+        fee = schoolFee[_level];
+
+        require (fee > 0, "can't pay Zero fee");
+        require(_level == 100 || _level == 200 || _level == 300 || _level == 400, "Invalid level");
+        require(token.balanceOf(msg.sender) >= fee, "Not enough funds to register");
+        
+        token.transferFrom(msg.sender, address(this), fee);
+
+        studentId = studentId + 1;
+        Student memory student = Student({
+            name: _name,
+            id: studentId,
+            studentAddress: msg.sender, 
+            level:_level, 
+            course: _course, 
+            status: Status.active,
+            paymentStatus: PaymentStatus.paid
+        });
+        students.push(student);
+        studentsAddresses.push(msg.sender);
+
+        emit RegistrationSuccessful (msg.sender, fee);
+        return true;
+    }
+
+    function ExpelStudent(address _address) internal {
+        for (uint i = 0; i < students.length; i++) {
+            if (students[i].studentAddress == _address) {
+                students[i] = students[students.length - 1];
+                students.pop();
+                break;
+            }
+        }
+    }
+
+    function EmployTeacher(string memory _name, uint16 _level, address _address) public returns(bool){
+        require(_level == 100 || _level == 200 || _level == 300 || _level == 400, "Invalid level");
+        
+        staffId ++;
+        staffs.push();
+        Staff storage staff = staffs[staffs.length - 1];
+        staff.teacherName = _name;
+        staff.id = staffId;
+        staff.staffAddress = _address;
+        staff.salary = staffSalary[_level];
+        staff.status = Status.active;
+
+        staffAddresses.push(_address);
+
+        return true;
+    }
+
+    function SackStaff(address _address) internal {
+        for (uint i = 0; i < staffs.length; i++) {
+            if (staffs[i].staffAddress == _address) {
+                staffs[i] = staffs[students.length - 1];
+                staffs.pop();
+                break;
+            }
+        }
+    }
+
+    function PayStaffSalary(address _address) public returns(bool){
+        require(_address != address(0));
+        
+        Staff storage staff = staffs[staffs.length - 1];
+
+        token.transfer(_address, staff.salary);
+
+        staff.lastPaidAt = block.timestamp;
+        emit PaymetSuccessful (_address, staff.salary);
+
+        return true;
+    }
+    function listStudents() public view returns(string[] memory){
+        string[] memory names = new string[](students.length);
+
+        for(uint i = 0; i < students.length; i++) {
+            names[i] = students[i].name;
+        }
+        return names;
+    }
+
+    function listStaffs() public view returns(string[] memory){
+        string[] memory names = new string[](staffs.length);
+
+        for(uint i = 0; i < staffs.length; i++) {
+            names[i] = staffs[i].teacherName;
+        }
+        return names;
+    }
+
+}
